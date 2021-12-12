9e7835b2748e2eb9a33eb8309f7c06c9e99def00
[aviv polak] <[avivp0437@gmail.com]>
2021-12-12 06:27:22 +0200

    9.17 pass

--- src/PatientorPage/PatientPage.tsx
+++ src/PatientorPage/PatientPage.tsx
@@ -1,7 +1,6 @@
 import { useParams } from "react-router";
 import React, { useEffect } from "react";
 import axios from "axios";
-import { async } from "q";
 import { useStateValue } from "../state";
 import { Patient } from "../types";
 import { Table } from "semantic-ui-react";
@@ -18,39 +17,35 @@ const PatientPage = () => {
     }
 
     useEffect(() => {
-        fetchData();
+        if (!state.updatedPatients[id]) fetchData();
     }, []);
     let patient = state.updatedPatients[id];
-    return (
+    return patient ? (
         <div className="App">
             <Table celled>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>Name</Table.HeaderCell>
-                        <Table.HeaderCell>Gender</Table.HeaderCell>
-                        <Table.HeaderCell>Occupation</Table.HeaderCell>
-                        <Table.HeaderCell>Health Rating</Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
                 <Table.Body>
-                    {Object.values(state.updatedPatients).map(
-                        (patient: Patient) => (
-                            <Table.Row key={patient.id}>
-                                <Table.Cell>{patient.name}</Table.Cell>
-                                <Table.Cell>{patient.gender}</Table.Cell>
-                                <Table.Cell>{patient.occupation}</Table.Cell>
-                                <Table.Cell>
-                                    <HealthRatingBar
-                                        showText={false}
-                                        rating={1}
-                                    />
-                                </Table.Cell>
-                            </Table.Row>
-                        )
-                    )}
+                    <Table.Row>
+                        <Table.Cell>
+                            {state.updatedPatients[id].name}
+                        </Table.Cell>
+                        <Table.Cell>
+                            {state.updatedPatients[id].gender}
+                        </Table.Cell>
+                        <Table.Cell>
+                            {state.updatedPatients[id].occupation}
+                        </Table.Cell>
+                        <Table.Cell>
+                            <HealthRatingBar showText={false} rating={1} />
+                        </Table.Cell>
+                        {/* {Object.values(patient.entries).map((entry: {}) => (
+                            <Table.Cell>{entry}</Table.Cell>
+                        ))} */}
+                    </Table.Row>
                 </Table.Body>
             </Table>
         </div>
+    ) : (
+        <div>nothing to show</div>
     );
 };
 
